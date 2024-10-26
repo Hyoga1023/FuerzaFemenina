0da45ffac596ff3d6dff562c869ce8b6ddb17bc9
Cesar Martinez <cesarmartinez99@gmail.com>
2024-10-25 19:38:46 -0500

    actualiza publicidad y enlaces

---
+++ script.js
@@ -0,0 +1,29 @@
+window.addEventListener('scroll', function() {
+    var introImg = document.getElementById('intro-img');
+    var contenido = document.getElementById('contenido');
+    var scrollPosition = window.scrollY;
+
+ 
+    if (scrollPosition > 0) {
+        introImg.style.opacity = 0; 
+        introImg.style.transform = 'scale(1)'; 
+        contenido.classList.add('mostrar'); 
+    } else {
+        introImg.style.opacity = 1; 
+        introImg.style.transform = 'scale(1.2)'; 
+        contenido.classList.remove('mostrar');
+    }
+});
+const ciudad = "Cali, Valle";
+
+function obtenerFecha() {
+    const opciones = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const fecha = new Date().toLocaleDateString('es-CO', opciones);
+    return fecha.charAt(0).toUpperCase() + fecha.slice(1);
+}
+
+function mostrarCiudadYFecha() {
+    const elemento = document.getElementById("info-fecha-ciudad");
+    elemento.innerHTML = `${ciudad}, ${obtenerFecha()}`;
+}
+mostrarCiudadYFecha();
